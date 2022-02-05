34c392653c823d976ceef613e0e5e6a6351a9081
eduardonk9999 <thiago.novak7@gmail.com>
2022-02-05 09:32:04 -0300

    Add, page interna de busca

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -3,6 +3,13 @@ import { Link } from 'react-router-dom';
 import { useStateValue } from '../Context/StateProvider';
 import useGoogleSearch from './useGoogleSearch';
 import GoogleImage from '../assets/googleimage.png'
+import SearchIcon from '@material-ui/icons/Search';
+import Description  from '@material-ui/icons/Description';
+import ImageIcon  from '@material-ui/icons/Image';
+import LocalOfferIcon  from '@material-ui/icons/LocalOffer';
+import RoomIcon  from '@material-ui/icons/Room';
+import MoreVertIcon  from '@material-ui/icons/MoreVert';
+
 
 import './SearchPage.css';
 import Search from './Search';
@@ -18,12 +25,45 @@ function SearchPage() {
     <div className="searchPage">
       <div className="searchPage_header">
         <Link to="/">
-          
           <img className="searchPage_logo" src={GoogleImage} alt="" />
         </Link>
+        <div className="searchPage_headerBody">
+         <Search hideButtons />
+         <div className="searchPage_options">
+          <div className="searchPage_optionsLeft">
+            <div className="searchPage_option">
+             <SearchIcon />
+             <Link to="/all">All </Link>
+            </div>
+            <div className="searchPage_option">
+             <Description />
+             <Link to="/news">News </Link>
+            </div>
+            <div className="searchPage_option">
+             <ImageIcon />
+             <Link to="/images">Images</Link>
+            </div>
+            <div className="searchPage_option">
+             <LocalOfferIcon />
+             <Link to="/shopping">shopping</Link>
+            </div>
+            <div className="searchPage_option">
+             <RoomIcon />
+             <Link to="/maps">maps</Link>
+            </div>
+            <div className="searchPage_option">
+             <MoreVertIcon />
+             <Link to="/more">more</Link>
+            </div>
+          </div>
+          <div className="searchPage_optionsRight">
+            
+          </div>
+         </div>
+        </div>
       </div>
       <div className="searchPage_results">
-       <Search hideButtons />
+
       </div>
     </div>
   );
